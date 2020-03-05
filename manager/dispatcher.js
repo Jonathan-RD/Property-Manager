fa8ec239e431b5fce2f9de5d299371a2944518dc
Jonathan-RD <jonathanrdas@gmail.com>
2020-03-05 08:54:08 -0500

    set up ping archetecture

--- manager/dispatcher.js
+++ manager/dispatcher.js
@@ -1,25 +1,34 @@
 const airbnb = require('airbnbapijs');
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+const apiKey = process.env.apiKey;
+const client = require('twilio')(process.env.accountsSid, process.env.authToken);
+var checksoutToday = [];
 
-const apiKey = process.env.apiKey
 
-
+/* Cron job that runs twice a day, it fetches all available crew members
+   then fetches all your properties which have guests checking out that day,
+   then crew members are notified about the properties in order to provide maintenance
+*/
 exports.handler = async (event, context) => {
-
-    const token = await getToken();
-
-    airbnb.setConfig({
-        defaultToken: token,
-        apiKey: apiKey
-    })
 
     try {
 
+        const token = await getToken();
+
+        airbnb.setConfig({
+            defaultToken: token,
+            apiKey: apiKey
+        })
+
+        const crewMembers = await getCrew()
+
+
         var data = await airbnb.getOwnActiveListings(token);
-        
+        var date = await getToday();
+
+        //TODO find better way to send batch sms, maybe swtich to AWS SNS
         data.array.forEach(async element => {
 
-            var date = await getToday();
             var currentListing = airbnb.getCalendar({
                 token: token,
                 id: element.id,
@@ -27,11 +36,16 @@ exports.handler = async (event, context) => {
                 endDate: date
             })
 
-            if (currentListing.checkout == date) {
-
-                sendPing()
-            }
+            if (currentListing.checkout == date)
+                checksoutToday.push(currentListing.address) //TODO verify if address is correct field
         });
+
+
+        crewMembers.array.forEach(async employee => {
+            await sendPing(employee.name, employee.number)
+        })
+
+
 
         return success;
     } catch (err) {
@@ -43,13 +57,39 @@ exports.handler = async (event, context) => {
 
 
 
-const sendPing = async () => {
+const sendPing = async (name, number) => {
 
+    try {
+        let msg = await client.messages.create({
+            body: `Hello, ${name}, hope all is well, these properties are ready for maintenance: ${JSON.stringify(checksoutToday).replace(/[\[\]']+/g, '').replace(/,/g, ', ')} (please let Jonny know if you are willing to work on a property)`
+            from: process.env.myNumber,
+            to: number
+        })
 
+        console.log(msg)
+    } catch (err) {
+        console.log(err)
+    }
 
 
 }
 
+
+// Function gets all online/available crew members
+const getCrew = async () => {
+    var params = {
+        TableName: 'CrewMembers',
+        ExpressionAttributeValues: {
+            ':s': true
+        }
+        FilterExpression: 'Online = :s'
+    }
+
+    var documentClient = new AWS.DynamoDB.DocumentClient();
+
+    return documentClient.scan(params).promise();
+
+}
 
 
 const getToday = async () => {
@@ -57,10 +97,10 @@ const getToday = async () => {
     var date = new Date();
 
     return date.getFullYear() + "-" + date.getMonth() + " " + date.getDay();
-    
+
 }
 
-
+// Function to get host's login token
 const getToken = async () => {
     try {
         var token = await airbnb.newAccessToken({
